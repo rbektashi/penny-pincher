9c0a8a885ccf8d266ded1646622a9d70cba80d26
mike <michaelwilsonsj@hotmail.com>
2020-10-29 15:58:59 -0400

    added burger navigation popup and category icons

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ submitBtn.addEventListener("click", (e) => {
   weeksContainer.classList.remove("display-none");
 });
 
+let formUsername = document.querySelector(".username-add");
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
@@ -18,6 +20,7 @@ form.addEventListener("submit", (e) => {
   let name = snapshot.get("username");
   let username = document.createElement("p");
   username.innerText = name;
+  formUsername.innerHTML = `<i class="fas fa-user-circle"></i> ${name}`;
   console.log(name);
 });
 
@@ -53,26 +56,33 @@ let weeks = [
 ];
 console.log(weeks);
 let weeksContainer = document.querySelector(".weeks-container");
+let slideout = document.querySelector(".slideout");
+let burgerNavigation = document.querySelector(".fa-hamburger");
+burgerNavigation.addEventListener("click", (e) => {
+  e.preventDefault();
+  slideout.classList.toggle("display-none");
+});
 
 let display = () => {
   weeks.forEach((item) => {
     let weeks = document.createElement("div");
     weeks.classList.add("week-block");
     let week = document.createElement("p");
+    week.classList.add("week-style");
     // week.innerText = item.week;
-    week.innerHTML = `${item.week} <i class="far fa-calendar-alt"> </i>`;
+    week.innerHTML = `<img class="ent-size calendar-img" src="assets/calendar.svg"/>${item.week}`;
     let entertainment = document.createElement("p");
     entertainment.innerText = item.entertainment;
-    entertainment.innerHTML = `${item.entertainment} <i class="far fa-calendar-alt"> </i>`;
+    entertainment.innerHTML = `<img class="ent-size" src="assets/ticket (2).svg"/> Entertainment: $${item.entertainment}`;
     let food = document.createElement("p");
     food.innerText = item.food;
-    // food.innerHTML = '<i class="far fa-calendar-alt"> </i>';
+    food.innerHTML = `<img class="ent-size" src="assets/vegetables.svg"/> Food: $${item.food}`;
     let clothing = document.createElement("p");
     clothing.innerText = item.clothing;
-    // clothing.innerHTML = '<i class="far fa-calendar-alt"> </i>';
+    clothing.innerHTML = `<img class="ent-size" src="assets/hoodie.svg"/> Clothing: $${item.clothing}`;
     let bills = document.createElement("p");
     bills.innerText = item.bills;
-    // bills.innerHTML = '<i class="far fa-calendar-alt"> </i>';
+    bills.innerHTML = `<img class="ent-size" src="assets/home.svg"/> Bills: $${item.bills}`;
     weeks.append(week, entertainment, food, clothing, bills);
     weeksContainer.append(weeks);
   });
